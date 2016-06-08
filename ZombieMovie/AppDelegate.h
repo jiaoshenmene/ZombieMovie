73f258fd762dda336afcd910e9eb39a9ba140719
apple <apple@appledemacbook-pro.local>
2016-06-08 16:29:03 +0800

    Initial Commit

---
+++ ZombieMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZombieMovie
+//
+//  Created by apple on 16/6/8.
+//  Copyright © 2016年 sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZombieMovie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZombieMovie
+//
+//  Created by apple on 16/6/8.
+//  Copyright © 2016年 sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
